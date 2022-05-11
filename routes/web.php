356724ab507f50910f1bfacbaa71cc213a1e0051
github newstage75 <rightkingdxanntume@gmail.com>
2022-05-11 09:02:23 +0900

    確認用にtranslationルートを設定

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('translation', function(){
+
+    echo trans('deepl.to_password_reminder');
+
+});
